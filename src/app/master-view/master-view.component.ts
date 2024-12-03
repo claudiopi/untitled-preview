fbbebda8102e81aeda8726ba85c51f7d0f7ef180
Claudio Pi <claudiopi@gmail.com>
2024-12-03 09:28:39 -0300

    untitled-app initial version

---
+++ src/app/master-view/master-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IGX_GRID_DIRECTIVES } from '@infragistics/igniteui-angular';
+
+@Component({
+  selector: 'app-master-view',
+  imports: [IGX_GRID_DIRECTIVES],
+  templateUrl: './master-view.component.html',
+  styleUrls: ['./master-view.component.scss']
+})
+export class MasterViewComponent {
+}
